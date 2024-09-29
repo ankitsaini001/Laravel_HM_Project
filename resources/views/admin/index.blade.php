5475c9cf9b67a5a54ff3dc66f33db79609ed8031
ankitsaini001 <ankitsaini594@gmail.com>
2024-09-29 20:13:51 +0530

    Integrate HTML Admin Template

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,27 +1,19 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-</head>
-<body>
-    <h1>Welcome Admin</h1>
-    <form action="{{route('logout')}}" method = 'POST'>
-        @csrf
-        <input type="submit" value='logout'>
-    </form>
-</body>
-</html> -->
+<!DOCTYPE html>
+<html>
+  @include('admin.css')
+  <body>
+    @include('admin.header')
+    @include('admin.sidebar')
 
-<!-- We use any of these methods for the logout 
-     Method 2 
--->
 
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-</x-app-layout>
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+            @include('admin.body')   
+        </div>
+      </div>
+    </div>
+    <!-- JavaScript files-->
+   @include('admin.js')
+  </body>
+</html>
