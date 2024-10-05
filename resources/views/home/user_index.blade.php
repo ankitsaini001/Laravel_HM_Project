92269e5124367fc98ecb69387419c424511d81f8
ankitsaini001 <ankitsaini594@gmail.com>
2024-10-05 19:11:54 +0530

    Integrate HTML User View Template

---
+++ resources/views/home/user_index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+@include('home.style')
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    
+   @include('home.header')
+   @include('home.about')
+   @include('home.gallery')
+   @include('home.book')
+
+   @include('home.blog')
+
+   @include('home.footer')
+
+</body>
+</html>
